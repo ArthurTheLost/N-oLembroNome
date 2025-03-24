a1c1151f2dd89f85778c6581d6610e176eb6148d
Arthur <140969772+ArthurTheLost@users.noreply.github.com>
2025-03-24 08:21:45 -0300

    o how

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
-    let TamanhoAtualFonte = 1
-    const AumentaFonteBotao = document.getElementById('aumentar-fonte')
+    let TamanhoAtualFonte = 1;
+
+    const AumentaFonteBotao = document.getElementById('aumentar-fonte');
+
+    AumentaFonteBotao.addEventListener('click', function(){
+        TamanhoAtualFonte +=0.1;
+        document.body.style.fontSize = 
+    })
 })
